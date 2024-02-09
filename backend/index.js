b44ec22fa791c839302758cbf795cf8708424cea
joseronaldo1 <ledezmaparrajoseronaldo@gmail.com>
2024-02-09 16:12:26 -0500

    merge devdfg

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 import express from'express'
 import  body_parsere from 'body-parser'
+import { rutaDeTipoRecurso } from './src/routes/TipoRecurso.route.js' 
+import { rutaDeVariedadCultivo } from './src/routes/VariedadCultivo.route.js'
 
 const servidor = express()
 
 servidor.use(body_parsere.json())
 servidor.use(body_parsere.urlencoded({extended: false}))
+
+servidor.use('/recursos', rutaDeTipoRecurso)
+servidor.use('/variedades', rutaDeVariedadCultivo)
 
 
 servidor.listen(3000, () =>{
