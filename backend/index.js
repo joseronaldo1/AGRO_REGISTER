2f9de9132e82d171b6d2024c84ed25eec5e4255c
joseronaldo1 <ledezmaparrajoseronaldo@gmail.com>
2024-02-20 07:48:32 -0500

    cambios actualizados de index

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,5 @@
 import express from'express'
 import  body_parsere from 'body-parser'
-import { rutaDeTipoRecurso } from './src/routes/TipoRecurso.route.js' 
-import { rutaDeVariedadCultivo } from './src/routes/VariedadCultivo.route.js'
 
 const servidor = express()
 
@@ -14,10 +12,6 @@ servidor.set('views','./views');
 servidor.get('/document',(req,res)=>{
     res.render('document.ejs');
 })
-
-servidor.use('/recursos', rutaDeTipoRecurso)
-servidor.use('/variedades', rutaDeVariedadCultivo)
-
 
 servidor.listen(3000, () =>{
     console.log("esta funcionando")
