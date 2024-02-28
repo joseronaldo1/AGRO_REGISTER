05834258bb36fd15da0469ff923cf02f3eee7041
pabloandres4112 <perdomo4112@gmail.com>
2024-02-28 12:29:32 -0500

    arreglado lo de index

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ const servidor = express();
 servidor.use(body_parser.json());
 servidor.use(body_parser.urlencoded({ extended: false }));
 
-servidor.use(rutaAsignaciones);
 
 servidor.listen(3000, () => {
     console.log("Está funcionando");
