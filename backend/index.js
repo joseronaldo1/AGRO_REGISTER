6dd86c2c5f44d43d3ee2b86d4c031891f17eda25
hellisghost <andrademahechafernando@gmail.com>
2024-02-28 07:39:10 -0600

    subida de correccion

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,10 @@
 import express from'express'
 import  body_parsere from 'body-parser'
-import programacion from './src/routes/programacion.routes.js'
 
 const servidor = express()
 
 servidor.use(body_parsere.json())
 servidor.use(body_parsere.urlencoded({extended: false}))
-
-servidor.use(programacion)
 
 servidor.listen(3000, () =>{
     console.log("esta funcionando")
